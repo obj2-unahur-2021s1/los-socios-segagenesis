05e8817d037b6ea17eee9cd3f97ce1611ea1eec4
reveke-hp <hernannicolaus@gmail.com>
2021-05-02 17:28:07 -0300

    Milles de fallas pero la idea esta ... creo

--- src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
@@ -1,4 +1,49 @@
 package ar.edu.unahur.obj2.socios
 
-class Cliente {
+class Cliente (val estadoActual = EstadosDelCliente, val viveEn = Barrios,val bolsillo = Int, val pedido: Pedido){
+    fun propina = EstadosDelCliente
+    fun valorPedido = Pedido
+}
+
+
+// esto puede ir en otro archivo pero no me deja crearlo
+class EstadosDelCliente(){
+    fun propina = Int
+}
+override class Enojado(): EstadosDelCliente{
+    fun propina = 0
+}
+override class Feliz() : EstadosDelCliente{
+    fun propina = valorPedido * 0.25
+}
+override class Indiferente(): EstadosDelCliente{
+    fun propina = cliente.bolsillo
+}
+override class Resfriado(): EstadosDelCliente{
+    fun propina = cliente.valorPedido
+}
+
+// idem anterior otro archivo
+
+class Pedido(val precio = Int){
+
+}
+
+// idem anterior
+
+class Barrios(){
+    fun influencia = Float
+}
+
+override class LasRosas() : Barrios{
+    fun influencia = 50
+}
+override class LasLauchas(): Barrios{
+    fun influencia = cliente.daPropina /2
+}
+override class BarrioVerde(): Barrios{
+    fun influencia = cliente.propina = 200
+}
+override class LasTorres(): Barrios{
+
 }
--- src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
@@ -4,5 +4,20 @@ import io.kotest.core.spec.style.DescribeSpec
 
 class ClienteTest : DescribeSpec({
   describe("Un/a cliente") {
+    val cliente = Cliente(Resfriado,LasLauchas,1000,pedido1)
+    val pedido1 = Pedido(1000)
+
+    describe("Prueba de propina"){
+      cliente.propina.shouldBe(500)
+    }
+    describe("Estado de animo del cliente"){
+      cliente.estadoActual().shouldBe(Resfriado)
+    }
+    describe("Cantidad de plata en el bolsillo del cliente"){
+      cliente.bolsillo.shouldBe(1000)
+    }
+    describe("Donde vive el cliente"){
+      cliente.viveEn.shouldBe(LasLauchas)
+    }
   }
 })
