33dad6169dffd2493a971a13cf67ff3ee918d6f1
reveke-hp <hernannicolaus@gmail.com>
2021-05-11 23:40:46 -0300

    Requisito 1 cumplido, 2 en proceso

--- src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/socios/Cliente.kt
@@ -1,8 +1,8 @@
 package ar.edu.unahur.obj2.socios
 
 class Cliente( var estadoDelCliente : EstadoAnimoCliente, var viveEn : Barrios, var dineroEnBolsillo: Int = 0) {
-    fun propina(importePedido: Int) = estadoDelCliente.propina(this,importePedido)
-    fun propinaConModificacionDeResidencia(importePedido: Int) = viveEn.modificador(this,importePedido)
+    fun propina(importePedido: Int) = estadoDelCliente.propina(this,importePedido) and viveEn.modificador(estadoDelCliente,this,importePedido)
+
 }
 abstract class EstadoAnimoCliente() {
     abstract fun propina(cliente: Cliente,importePedido:Int):Int
@@ -13,30 +13,30 @@ object Enojado : EstadoAnimoCliente() {
 }
 
 object Feliz : EstadoAnimoCliente() {
-    override fun propina(cliente: Cliente,importePedido:Int) = (importePedido * 0.25).toInt()
+    override fun propina(cliente: Cliente,importePedido:Int) = ((importePedido * 0.25).toInt())
 }
 
 object Indiferente: EstadoAnimoCliente() {
-    override fun propina(cliente: Cliente,importePedido:Int) = cliente.dineroEnBolsillo
+    override fun propina(cliente: Cliente,importePedido:Int) = (cliente.dineroEnBolsillo)
 }
 
 object Resfriado: EstadoAnimoCliente() {
-    override fun propina(cliente: Cliente,importePedido: Int) = importePedido
+    override fun propina(cliente: Cliente,importePedido: Int) = (importePedido)
 }
 
 
 abstract class Barrios(){
-    abstract fun modificador(cliente: Cliente,importePedido: Int):Int
+    abstract fun modificador(estadoDelCliente: EstadoAnimoCliente,cliente: Cliente,importePedido: Int):Int
 }
 object LasRosas: Barrios(){
-    override fun modificador(cliente: Cliente,importePedido: Int) = cliente.propina(importePedido) + 50
+    override fun modificador(estadoDelCliente: EstadoAnimoCliente,cliente: Cliente,importePedido: Int) = estadoDelCliente.propina(cliente,importePedido)
 }
 object LasLauchas: Barrios(){
-    override fun modificador(cliente:Cliente,importePedido: Int) = cliente.propina(importePedido) / 2
+    override fun modificador(estadoDelCliente: EstadoAnimoCliente,cliente:Cliente,importePedido: Int) = (estadoDelCliente.propina(cliente,importePedido))* 0.5.toInt()
 }
 object BarrioVerde: Barrios() {
-    override fun modificador(cliente: Cliente, importePedido: Int) = 200
+    override fun modificador(estadoDelCliente: EstadoAnimoCliente,cliente: Cliente, importePedido: Int) = 200
 }
 object LasTorres : Barrios() {
-    override fun modificador(cliente: Cliente,importePedido: Int) = cliente.propina(importePedido)
+    override fun modificador(estadoDelCliente: EstadoAnimoCliente,cliente: Cliente,importePedido: Int) = estadoDelCliente.propina(cliente,importePedido)
 }
--- src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/socios/ClienteTest.kt
@@ -18,18 +18,22 @@ class ClienteTest : DescribeSpec({
     }
     describe("Propina dependiendo su estado de animo"){
       it("Cuando esta enojado ") {
+        cliente.viveEn = LasTorres
         cliente.estadoDelCliente = Enojado
         cliente.propina(100).shouldBe(0)
       }
       it("Cuando esta feliz") {
+        cliente.viveEn = LasTorres
         cliente.estadoDelCliente = Feliz
         cliente.propina(100).shouldBe(25)
       }
       it("Cuando esta indiferente") {
+        cliente.viveEn = LasTorres
         cliente.estadoDelCliente = Indiferente
         cliente.propina(100).shouldBe(1000)
       }
       it("Cuando esta resfriado") {
+        cliente.viveEn = LasTorres
         cliente.estadoDelCliente = Resfriado
         cliente.propina(100).shouldBe(100)
       }
@@ -37,23 +41,23 @@ class ClienteTest : DescribeSpec({
     describe("Propina dependiendo de donde vive"){
       it("Si Vive en Las Rosas"){
         cliente.viveEn = LasRosas
-        cliente.estadoDelCliente = Resfriado
-        cliente.propinaConModificacionDeResidencia(100).shouldBe(150)
+        cliente.estadoDelCliente = Feliz
+        cliente.propina(100).shouldBe(75)
       }
       it("Si Vive en Las Lauchas"){
         cliente.viveEn = LasLauchas
-        cliente.estadoDelCliente = Resfriado
-        cliente.propinaConModificacionDeResidencia(100).shouldBe(50)
+        cliente.estadoDelCliente = Feliz
+        cliente.propina(100).shouldBe(12.5)
       }
       it("Si Vive en Barrio Verde"){
         cliente.viveEn = BarrioVerde
-        cliente.estadoDelCliente = Resfriado
-        cliente.propinaConModificacionDeResidencia(100).shouldBe(200)
+        cliente.estadoDelCliente = Feliz
+        cliente.propina(100).shouldBe(200)
       }
       it("Si Vive en Las Torres"){
         cliente.viveEn = LasTorres
-        cliente.estadoDelCliente = Resfriado
-        cliente.propinaConModificacionDeResidencia(100).shouldBe(100)
+        cliente.estadoDelCliente = Feliz
+        cliente.propina(100).shouldBe(100)
       }
     }
   }
